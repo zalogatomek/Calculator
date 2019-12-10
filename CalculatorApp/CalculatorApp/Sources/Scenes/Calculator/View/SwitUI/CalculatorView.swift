188f6762d7e00528a589473a60a418eb9502e82d
Tomasz Załoga <tomasz.zaloga@siroccomobile.com>
2019-12-10 16:03:43 +0100

    [WIP] Extract Calculator SwiftUI's content view to separate view

--- CalculatorApp/CalculatorApp/Sources/Scenes/Calculator/View/SwitUI/CalculatorView.swift
+++ CalculatorApp/CalculatorApp/Sources/Scenes/Calculator/View/SwitUI/CalculatorView.swift
@@ -12,14 +12,11 @@ struct CalculatorView: View {
     
     // MARK: - Configuration
     
-    private let rowSpacing: CGFloat = 8.0
-    private let columnSpacing: CGFloat = 8.0
     private let padding: CGFloat = 20.0
     
     // MARK: - ViewModel
     
     private let viewModel = CalculatorViewModel()
-    private let inputLayout = CalculatorInputLayout()
     @State private var result: String = ""
     
     // MARK: - Lifecycle
@@ -38,43 +35,71 @@ struct CalculatorView: View {
             Color(ThemeStore.current.backgroundColor)
                 .edgesIgnoringSafeArea(.all)
             
-            VStack(alignment: .center, spacing: rowSpacing) {
-                ZStack {
-                    Image("Logo")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: 200.0, height: nil)
-                }
-                .frame(minWidth: 0.0, maxWidth: .infinity,
-                       minHeight: 0.0, maxHeight: .infinity)
-                
-                Text(result)
-                    .font(.title)
-                    .foregroundColor(Color(ThemeStore.current.textLight))
-                    .frame(minWidth: 0.0, maxWidth: .infinity,
-                           minHeight: 0.0, maxHeight: .infinity,
-                           alignment: .trailing)
-                
-                ForEach(inputLayout.rows, id: \.self) { row in
-                    GeometryReader { geometry in
-                        HStack(alignment: .center, spacing: self.columnSpacing) {
-                            ForEach(row, id: \CalculatorInputType.name) { inputType in
-                                CalculatorButtonRepresentable(
-                                    name: inputType.name,
-                                    kind: CalculatorButtonKindMapper.map(inputType: inputType),
-                                    action: {
-                                        self.viewModel.append(inputType)
-                                })
-                                .frame(width: self.widthForButton(inRow: row, withType: inputType, geometry: geometry))
-                            }
-                        }
-                    }
-                }
-            }
+            CalculatorContentView(result: $result, buttonAction: { inputType in
+                self.viewModel.append(inputType)
+            })
             .padding(padding)
         }
         .onAppear {
             self.bindViewModel()
+        }
+    }
+}
+
+struct CalculatorView_Previews: PreviewProvider {
+    static var previews: some View {
+        CalculatorView()
+    }
+}
+
+struct CalculatorContentView: View {
+    
+    // MARK: - Configuration
+    
+    private let rowSpacing: CGFloat = 8.0
+    private let columnSpacing: CGFloat = 8.0
+    private let inputLayout = CalculatorInputLayout()
+    
+    // MARK: - Properties
+    
+    @Binding var result: String
+    var buttonAction: (CalculatorInputType) -> Void
+    
+    // MARK: - Body
+    
+    var body: some View {
+        VStack(alignment: .center, spacing: rowSpacing) {
+            ZStack {
+                Image("Logo")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 200.0, height: nil)
+            }
+            .frame(minWidth: 0.0, maxWidth: .infinity,
+                   minHeight: 0.0, maxHeight: .infinity)
+            
+            Text(result)
+                .font(.title)
+                .foregroundColor(Color(ThemeStore.current.textLight))
+                .frame(minWidth: 0.0, maxWidth: .infinity,
+                       minHeight: 0.0, maxHeight: .infinity,
+                       alignment: .trailing)
+            
+            ForEach(inputLayout.rows, id: \.self) { row in
+                GeometryReader { geometry in
+                    HStack(alignment: .center, spacing: self.columnSpacing) {
+                        ForEach(row, id: \CalculatorInputType.name) { inputType in
+                            CalculatorButtonRepresentable(
+                                name: inputType.name,
+                                kind: CalculatorButtonKindMapper.map(inputType: inputType),
+                                action: {
+                                    self.buttonAction(inputType)
+                                })
+                                .frame(width: self.widthForButton(inRow: row, withType: inputType, geometry: geometry))
+                        }
+                    }
+                }
+            }
         }
     }
     
@@ -95,11 +120,5 @@ struct CalculatorView: View {
         } else {
             return geometry.size.width - spacing - ((columns - 1) * singleButtonWidth)
         }
-    }
-}
-
-struct CalculatorView_Previews: PreviewProvider {
-    static var previews: some View {
-        CalculatorView()
     }
 }
