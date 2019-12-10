58ba7aaae9086b2d7c2484a68413488387863d24
Tomasz Załoga <tomasz.zaloga@siroccomobile.com>
2019-12-10 13:48:04 +0100

    Add RxSwift and RxCocoa to project

---
+++ CalculatorApp/CalculatorApp/Sources/Scenes/Calculator/ViewModel/Rx/CalculatorViewModelRx.swift
@@ -0,0 +1,39 @@
+//
+//  CalculatorViewModelRx.swift
+//  CalculatorApp
+//
+//  Created by Tomasz Załoga on 10/12/2019.
+//  Copyright © 2019 Tomasz Załoga. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+import RxCocoa
+
+struct CalculatorViewModelRx {
+    
+    // MARK: - Properties
+    
+    private let viewModel: CalculatorViewModel = CalculatorViewModel()
+    
+    // MARK: - Lifecycle
+    
+    init() {
+        setupViewModel()
+    }
+    
+    private func setupViewModel() {
+        result.accept(viewModel.result)
+        viewModel.onResultChanged = { result in
+            self.result.accept(result)
+        }
+    }
+    
+    // MARK: - Input
+    
+    var input: PublishSubject<CalculatorInputType> = PublishSubject()
+    
+    // MARK: - Output
+    
+    var result: BehaviorRelay<String> = BehaviorRelay(value: "")
+}
